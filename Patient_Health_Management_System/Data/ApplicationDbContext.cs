72b49eae76dcfcc1781cc50bdf4dddbfe0757f75
lequocanh2684 <anh.lq192684@sis.hust.edu.vn>
2023-04-04 20:38:44 +0700

    first initial

---
+++ Patient_Health_Management_System/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Patient_Health_Management_System.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
