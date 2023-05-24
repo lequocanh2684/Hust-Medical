411a8a294ab92b10372f6527c548fd86cbba2575
lequocanh2684 <anh.lq192684@sis.hust.edu.vn>
2023-05-25 00:57:37 +0700

    design main page

---
+++ Patient_Health_Management_System/Domain/Models/NavItem.cs
@@ -0,0 +1,10 @@
+﻿using MudBlazor;
+
+namespace Patient_Health_Management_System.Domain.Models
+{
+    public class NavItem
+    {
+        public string ItemContent { get; set; }
+        public MudNavLink NavLinkItem { get; set; }
+    }
+}
